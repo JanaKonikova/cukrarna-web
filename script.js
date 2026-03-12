f676e39edeb7adfa396b7ada98fcdfdd6107dab4
JanaKonikova <janakonikova@seznam.cz>
2026-03-12 16:15:41 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll pro všechny odkazy v navigaci
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
