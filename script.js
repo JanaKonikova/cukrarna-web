d283003d9a90d3c25ef2617bdccf257c3359629d
JanaKonikova <janakonikova@seznam.cz>
2026-03-12 16:46:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// Hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+hamburger.addEventListener('click', ()=>{navMenu.classList.toggle('show');});
+
 // Smooth scroll
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', e => {
+document.querySelectorAll('nav a').forEach(link=>{
+    link.addEventListener('click',e=>{
         e.preventDefault();
-        document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+        document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+        navMenu.classList.remove('show'); // zavře menu po kliknutí
     });
 });
