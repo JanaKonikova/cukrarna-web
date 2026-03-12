624300f8609b70fdde1187da60c881c61e1e017a
JanaKonikova <janakonikova@seznam.cz>
2026-03-12 17:32:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ menu.classList.remove("active")
 })
 
 })
+// smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
+anchor.addEventListener('click', function (e) {
+
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute('href'));
+
+if(target){
+target.scrollIntoView({
+behavior: "smooth"
+});
+}
+
+});
+
+});
